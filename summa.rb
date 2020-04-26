4632be20bcaaff91bf6d6473c6f493737305f7f6
Shushucod <64014588+Shushucod@users.noreply.github.com>
2020-04-26 20:44:05 +0300

    Update summa.rb

--- summa.rb
+++ summa.rb
@@ -1,1 +1,18 @@
+loop do
+  puts 'Введите название товара'
+  tovar = gets.chomp
+  break if tovar == 'стоп'
+  puts 'Введите цену за 1 товар'
+  price = gets.chomp.to_i
+  puts 'Введите количество товара'
+  count = gets.chomp.to_i
+  
+  list[tovar] = { price: price, count: count }
+  cost = price * count
+  puts 'Товар- #{tovar}, цена- #{price}, кол-во- #{count}, стоимость- #{cost}'
+  total_cost += cost
+  puts 'Итоговая стоимость #{total_cost}'
+  
+  
+
 
